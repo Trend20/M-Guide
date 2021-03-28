7e479377f3788ef6e3467a34805cc23e300c8b7e
Trend20 <enockomondi305@gmail.com>
2021-03-28 05:29:53 +0300

    fixing the modal

--- js/main.js
+++ js/main.js
@@ -2,44 +2,39 @@
 let myButton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    myButton.style.display = "block";
-  } else {
-    myButton.style.display = "none";
-  }
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        myButton.style.display = "block";
+    } else {
+        myButton.style.display = "none";
+    }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; 
-  document.documentElement.scrollTop = 0; 
-} 
-
-
-// callback modal
-const callbackBtn = document.getElementById('request');
-
-// add eventListner
-
-callbackBtn.addEventListener('click', showModal);
-
-// declare the showMOdal function
-
-const showModal = () =>{
-
-    let modalContainer = document.createElement('div');
-    modalContainer.innerHTML = `
-      <h3><b>Request</b> a Callback</h3>
-      <p>We can call you in 30 seconds, just enter your number.</p>
-      <form id="request-form">
-        <input type="text" placeholder=" Name*" />
-        <br />
-        <input type="tel" placeholder="Phone*" />
-        <br />
-        <button type="submit"> Request </button>
-      </form>
-    `;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
+
+
+
+// modal
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector(".trigger");
+const closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
